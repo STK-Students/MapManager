85ee50bbd6388d0945a5b528bdd115624879bfc2
saltyaimbotter <saltyaimbotter@gmail.com>
2022-06-21 11:15:11 +0200

    replace CDN links with local dependencies

--- webRoot/templates/home/map/removeMap.php
+++ webRoot/templates/home/map/removeMap.php
@@ -20,12 +20,9 @@ if(isset($_POST['submit_map_form'])){
 <html>
 <head>
     <title>Gruppe löschen</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js"
-            integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT"
-            crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../../../dependencies/Bootstrap/css/bootstrap.min.css">
+    <script src="../../../dependencies/Bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../../../dependencies/jQuery/jQuery.js"></script>
     <style>
         body{
             background-color: #DC3545;
