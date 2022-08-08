4748d9ee69235858df3e4909460738bbc32a0e82
saltyaimbotter <saltyaimbotter@gmail.com>
2022-08-08 07:56:40 +0200

    fix MapFileParser sets SCALEDENOM in layer instead of MAP

--- webRoot/dependencies/MapFileParser/Parser/Map.php
+++ webRoot/dependencies/MapFileParser/Parser/Map.php
@@ -11,6 +11,7 @@ declare(strict_types=1);
 
 namespace MapFile\Parser;
 
+require_once $_SERVER['DOCUMENT_ROOT'] . "/dependencies/MapFileParser/Exception/UnsupportedException.php";
 use MapFile\Exception\UnsupportedException;
 use MapFile\Model\Map as MapObject;
 
@@ -126,7 +127,7 @@ class Map extends Parser
 
                 $this->currentLineIndex = $scalebarParser->lineEnd;
             } elseif ($this->parsing === 'MAP' && preg_match('/^SCALEDENOM ([0-9]+(?:\.(?:[0-9]+))?)$/i', $line, $matches)) {
-                $layer->scaledenom = floatval($matches[1]);
+                $map->scaledenom = floatval($matches[1]);
             } elseif ($this->parsing === 'MAP' && preg_match('/^SHAPEPATH ["\'](.+)["\']$/i', $line, $matches)) {
                 $map->shapepath = $matches[1];
             } elseif ($this->parsing === 'MAP' && preg_match('/^SIZE ([0-9]+) ([0-9]+)$/i', $line, $matches)) {
