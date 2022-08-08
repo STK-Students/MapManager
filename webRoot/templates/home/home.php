c8cf0f7cc7915747f081ee2e90fbe433c9972320
saltyaimbotter <saltyaimbotter@gmail.com>
2022-08-08 08:08:23 +0200

    fix wrong title for service edit modal

--- webRoot/templates/home/home.php
+++ webRoot/templates/home/home.php
@@ -197,7 +197,7 @@ $currentGroup = $_SESSION['currentGroup'];
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="modalTitleEditService">Dienst erstellen</h5>
+                <h5 class="modal-title" id="modalTitleEditService">Dienst bearbeiten</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <form name="edit_service_form" action="./map/editMap.php" method="post">
